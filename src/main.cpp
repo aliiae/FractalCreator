b5517ea1022ad16f2ef50c8043723fa34f8fc70d
Aliia <26248907+aliiae@users.noreply.github.com>
2020-05-05 21:12:48 +0100

    Update user input parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,48 @@
 #include "Creator.h"
 #include <iostream>
-#include <unordered_map>
-const int MaxIterations = 256;
-const int ImageWidth = 1000;
-const int ImageHeight = 1000;
+#include <BurningShip.h>
+#include <MandelbrotSet.h>
+#include <map>
 
 int main() {
+  const int MaxIterations = 256;
+  const int ImageWidth = 1000;
+  const int ImageHeight = 1000;
+  const std::string OutputPath = "output.ppm";
+  char DefaultChoice = 'M';
+
+  std::map<char, std::function<std::unique_ptr<Fractal>()>> InputLetterMap =
+	  {
+		  {DefaultChoice, [MaxIterations]() { return std::make_unique<MandelbrotSet>(MaxIterations); }},
+		  {'B', [MaxIterations]() { return std::make_unique<BurningShip>(MaxIterations); }}
+	  };
+
   std::string SelectedTypeLetter;
   int SelectedOrder;
   std::unique_ptr<Fractal> SelectedFractal;
   std::cout << "Please choose the fractal type [M,B]:"
-            << "\n"
-            << "1. [M]andelbrot Set (default)"
-            << "\n"
-            << "2. [B]urning Ship" << std::endl;
+			<< "\n"
+			<< "1. [M]andelbrot Set (default)"
+			<< "\n"
+			<< "2. [B]urning Ship" << std::endl;
   std::cin >> SelectedTypeLetter;
-  if (SelectedTypeLetter.at(0) != 'B') {
-    if (SelectedTypeLetter.at(0) != 'M') {
-      std::cout << "Setting the fractal type to the default value M"
-                << std::endl;
-    }
-    SelectedFractal = std::make_unique<MandelbrotSet>(MaxIterations);
+  if (InputLetterMap.find(SelectedTypeLetter.at(0)) != InputLetterMap.end()) {
+	SelectedFractal = InputLetterMap[SelectedTypeLetter.at(0)]();
   } else {
-    SelectedFractal = std::make_unique<BurningShip>(MaxIterations);
+	std::cout << "Setting the fractal type to the default value " << DefaultChoice
+			  << std::endl;
+	SelectedFractal = InputLetterMap[DefaultChoice]();
   }
+
   std::cout << "Please choose the fractal order (default 2):" << std::endl;
   std::cin >> SelectedOrder;
   if (SelectedOrder <= 0) {
-    std::cout << "Setting the order to the default value 2" << std::endl;
-    SelectedOrder = 2;
+	std::cout << "Setting the order to the default value 2" << std::endl;
+	SelectedOrder = 2;
   }
   SelectedFractal->setOrder(SelectedOrder);
-  Creator FractalCreator = Creator("output.ppm", ImageWidth, ImageHeight,
-                                   MaxIterations, std::move(SelectedFractal));
+  Creator FractalCreator = Creator(OutputPath, ImageWidth, ImageHeight,
+								   MaxIterations, std::move(SelectedFractal));
   FractalCreator.draw();
   return 0;
 }
