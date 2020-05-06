82f119737abb5911261fe7ac4d81e3a7c8b77550
Aliia <26248907+aliiae@users.noreply.github.com>
2020-05-06 09:06:51 +0100

    Add SDL

--- include/Area.h
+++ include/Area.h
@@ -1,20 +1,40 @@
 #ifndef FRACTALCREATOR_INCLUDE_AREA_H
 #define FRACTALCREATOR_INCLUDE_AREA_H
+#include <string>
 
-template <typename T> class Area {
+template<typename T> class Area {
 public:
   [[maybe_unused]] Area(T XMin, T XMax, T YMin, T YMax)
-      : x_min(XMin), x_max(XMax), y_min(YMin), y_max(YMax){};
+	  : x_min_(XMin), x_max_(XMax), y_min_(YMin), y_max_(YMax) {};
 
-  T width() { return (x_max - x_min); }
-  T height() { return (y_max - y_min); }
+  T width() { return (x_max_ - x_min_); }
+  T height() { return (y_max_ - y_min_); }
 
-  T getXMin() const { return x_min; }
-  T getYMin() const { return y_min; }
-  void setYMax(T YMax) { y_max = YMax; }
+  T getXMin() const { return x_min_; }
+  T getYMin() const { return y_min_; }
+  void setYMax(T YMax) { y_max_ = YMax; }
+  void setXMin(T XMin) {
+	x_min_ = XMin;
+  }
+  T getXMax() const {
+	return x_max_;
+  }
+  void setXMax(T XMax) {
+	x_max_ = XMax;
+  }
+  void setYMin(T YMin) {
+	y_min_ = YMin;
+  }
+  T getYMax() const {
+	return y_max_;
+  }
+  std::string toString() {
+//    TODO:
+	return "(" + std::to_string(x_min_) + "," + std::to_string(x_max_) + "," + std::to_string(y_min_) + " " + std::to_string(y_max_) + ")";
+  }
 
 private:
-  T x_min, x_max, y_min, y_max;
+  T x_min_, x_max_, y_min_, y_max_;
 };
 
 #endif // FRACTALCREATOR_INCLUDE_AREA_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,66 @@
+#include <Renderer.h>
+#include <Controller.h>
 #include "Creator.h"
-#include <iostream>
-#include <unordered_map>
-const int MaxIterations = 256;
-const int ImageWidth = 1000;
-const int ImageHeight = 1000;
 
-int main() {
-  std::string SelectedTypeLetter;
+std::shared_ptr<Fractal> selectFractal(std::string &SelectedTypeLetter, int MaxIterations) {
   int SelectedOrder;
-  std::unique_ptr<Fractal> SelectedFractal;
+  std::shared_ptr<Fractal> SelectedFractal;
   std::cout << "Please choose the fractal type [M,B]:"
-            << "\n"
-            << "1. [M]andelbrot Set (default)"
-            << "\n"
-            << "2. [B]urning Ship" << std::endl;
+			<< "\n"
+			<< "1. [M]andelbrot Set (default)"
+			<< "\n"
+			<< "2. [B]urning Ship" << std::endl;
   std::cin >> SelectedTypeLetter;
-  if (SelectedTypeLetter.at(0) != 'B') {
-    if (SelectedTypeLetter.at(0) != 'M') {
-      std::cout << "Setting the fractal type to the default value M"
-                << std::endl;
-    }
-    SelectedFractal = std::make_unique<MandelbrotSet>(MaxIterations);
+  if (SelectedTypeLetter.at(0)!='B') {
+	if (SelectedTypeLetter.at(0)!='M') {
+	  std::cout << "Setting the fractal type to the default value M"
+				<< std::endl;
+	}
+	SelectedFractal = std::make_shared<MandelbrotSet>(MaxIterations);
   } else {
-    SelectedFractal = std::make_unique<BurningShip>(MaxIterations);
+	SelectedFractal = std::make_shared<BurningShip>(MaxIterations);
   }
   std::cout << "Please choose the fractal order (default 2):" << std::endl;
   std::cin >> SelectedOrder;
   if (SelectedOrder <= 0) {
-    std::cout << "Setting the order to the default value 2" << std::endl;
-    SelectedOrder = 2;
+	std::cout << "Setting the order to the default value 2" << std::endl;
+	SelectedOrder = 2;
   }
   SelectedFractal->setOrder(SelectedOrder);
-  Creator FractalCreator = Creator("output.ppm", ImageWidth, ImageHeight,
-                                   MaxIterations, std::move(SelectedFractal));
-  FractalCreator.draw();
+  return SelectedFractal;
+}
+
+int main() {
+  const int MaxIterations = 512;
+  const int ImageWidth = 640;
+  const int ImageHeight = 640;
+
+  std::string SelectedTypeLetter;
+  std::shared_ptr<Fractal> SelectedFractal = selectFractal(SelectedTypeLetter, MaxIterations);
+  Creator FractalCreator = Creator( ImageWidth, ImageHeight,
+								   MaxIterations, SelectedFractal);
+  Renderer Renderer(ImageWidth, ImageHeight);
+  Controller Controller(SelectedFractal);
+
+  std::vector<unsigned char> NewPixels = FractalCreator.getPixels();
+  Renderer.render(NewPixels);
+  Renderer.updateWindowTitle(SelectedFractal->getZoomArea());
+
+  bool Running{true};
+  SDL_Event Event;
+  while (Running) {
+	while (SDL_PollEvent(&Event)) {
+	  if (Event.type==SDL_QUIT) {
+		Running = false;
+		break;
+	  } else if (Event.type==SDL_KEYDOWN) {
+		if (Controller.changeZoom(Event)) {
+		  NewPixels = FractalCreator.getPixels();
+		  Renderer.render(NewPixels);
+		  Renderer.updateWindowTitle(SelectedFractal->getZoomArea());
+		}
+	  }
+	}
+  }
   return 0;
 }
