2f03105a6c5a7199e5931c4b71456bfaec381e17
Aliia <26248907+aliiae@users.noreply.github.com>
2020-05-06 15:24:16 +0100

    Exit program on pressing Esc

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <map>
 
 std::shared_ptr<Fractal> selectFractalType(int MaxIterations);
+bool escape(const SDL_Event &Event);
 int main() {
   const int MaxIterations = 256;
   const int ImageWidth = 800;
@@ -14,11 +15,11 @@ int main() {
 
   std::shared_ptr<Fractal> SelectedFractal = selectFractalType(MaxIterations);
   Creator FractalCreator =
-      Creator(ImageWidth, ImageHeight, MaxIterations, SelectedFractal);
+	  Creator(ImageWidth, ImageHeight, MaxIterations, SelectedFractal);
   Renderer Renderer(ImageWidth, ImageHeight);
   Controller Controller(SelectedFractal);
   const std::string &Title =
-      SelectedFractal->getName() + " " + SelectedFractal->getZoomAreaString();
+	  SelectedFractal->getName() + " " + SelectedFractal->getZoomAreaString();
 
   std::vector<unsigned char> NewPixels = FractalCreator.getPixels();
   Renderer.render(NewPixels);
@@ -27,44 +28,45 @@ int main() {
   bool Running{true};
   SDL_Event Event;
   while (Running) {
-    while (SDL_PollEvent(&Event)) {
-      if (Event.type == SDL_QUIT) {
-        Running = false;
-        break;
-      } else if (Event.type == SDL_KEYDOWN) {
-        if (Controller.changeZoom(Event)) {
-          NewPixels = FractalCreator.getPixels();
-          Renderer.render(NewPixels);
-          Renderer.updateWindowTitle(Title);
-        }
-      }
-    }
+	while (SDL_PollEvent(&Event)) {
+	  if (escape(Event)) {
+		Running = false;
+		break;
+	  } else if (Event.type==SDL_KEYDOWN) {
+		if (Controller.changeZoom(Event)) {
+		  NewPixels = FractalCreator.getPixels();
+		  Renderer.render(NewPixels);
+		  Renderer.updateWindowTitle(Title);
+		}
+	  }
+	}
   }
   return 0;
 }
+bool escape(const SDL_Event &Event) { return Event.type==SDL_QUIT || (Event.type==SDL_KEYDOWN && Event.key.keysym.sym==SDLK_ESCAPE); }
 
 std::shared_ptr<Fractal> selectFractalType(const int MaxIterations) {
   char DefaultChoice = 'M';
   std::string SelectedTypeLetter;
   std::__1::map<char, std::function<std::shared_ptr<Fractal>()>>
-      InputLetterMap = {{DefaultChoice,
-                         [MaxIterations]() {
-                           return std::make_shared<MandelbrotSet>(
-                               MaxIterations);
-                         }},
-                        {'B', [MaxIterations]() {
-                           return std::make_shared<BurningShip>(MaxIterations);
-                         }}};
+	  InputLetterMap = {{DefaultChoice,
+						 [MaxIterations]() {
+						   return std::make_shared<MandelbrotSet>(
+							   MaxIterations);
+						 }},
+						{'B', [MaxIterations]() {
+						  return std::make_shared<BurningShip>(MaxIterations);
+						}}};
   std::__1::unique_ptr<Fractal> SelectedFractal;
   std::cout << "Please choose the fractal type (default " << DefaultChoice
-            << "):\n"
-            << "1. [M]andelbrot Set\n"
-            << "2. [B]urning Ship" << std::endl;
+			<< "):\n"
+			<< "1. [M]andelbrot Set\n"
+			<< "2. [B]urning Ship" << std::endl;
   std::cin >> SelectedTypeLetter;
-  if (InputLetterMap.find(SelectedTypeLetter.at(0)) != InputLetterMap.end()) {
-    return InputLetterMap[SelectedTypeLetter.at(0)]();
+  if (InputLetterMap.find(SelectedTypeLetter.at(0))!=InputLetterMap.end()) {
+	return InputLetterMap[SelectedTypeLetter.at(0)]();
   }
   std::cout << "Setting the fractal type to the default value " << DefaultChoice
-            << std::endl;
+			<< std::endl;
   return InputLetterMap[DefaultChoice]();
 }
